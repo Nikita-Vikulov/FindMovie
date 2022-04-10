1783d253cfca9e24e1004d6f2b77d8dcd7e25fe5
Nikita-Vikulov <n.vikulov92@yandex.ru>
2022-04-11 02:20:55 +0300

    commit

--- app/src/main/java/com/example/findmovie/data/api/MovieResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.findmovie.data.api
-
-import com.example.findmovie.data.model.Movie
-
-data class MovieResponse(
-    val results: List<Movie>,
-)
--- app/src/main/java/com/example/findmovie/data/api/TelevisionResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.findmovie.data.api
-
-import com.example.findmovie.data.model.Television
-
-data class TelevisionResponse(
-    val results: List<Television>,
-)
